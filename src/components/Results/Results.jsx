372ccb03815a6bdeb6a3b3430b292ef1796ca274
Cole Essig <hoopwalker@yahoo.com>
2025-02-25 12:40:52 -0700

    added animations and hover effects for mpost ui elements

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -4,6 +4,7 @@ function Results({ randomPick, madeChoice }) {
     return (
         <div className={`results ${madeChoice ? 'results_on' : ''}` }>
           <p className='results__display'>{randomPick}</p>
+          <div className='results results__display_pulse_animation'></div>
         </div>
 
     )
